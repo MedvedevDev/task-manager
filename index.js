a0fc289564ffdd26fbcbca748c3cb874656674f7
MedvedevDev <a95.medvedev@gmail.com>
2023-07-01 01:25:43 +0300

    Refactor with async await

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./src/db/mongoose.js') //to run file and connect to db
 const User = require('./src/models/user')
 const Task = require('./src/models/task')
+const trace_events = require("trace_events");
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,64 +10,116 @@ const port = process.env.PORT || 3000
 // configure express to automatically parse incoming JSON to Object
 app.use(express.json())
 
-app.post('/users', (req, res) => {
+// Add new user
+app.post('/users', async (req, res) => {
     const user = new User(req.body);
-    user.save().then((user) => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
+    try {
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+    // user.save().then((user) => {
+    //     res.status(201).send(user)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then(users => {
+// Get all users
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({});
         res.status(200).send(users);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
+
+    // User.find({}).then(users => {
+    //     res.status(200).send(users);
+    // }).catch(error => {
+    //     res.status(500).send(error);
+    // })
 })
 
-app.get('/users/:id', (req, res) => {
+// Get user by ID
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
-    User.findById(_id).then(user => {
-        if(!user) {
+    try {
+        const user = await User.findById(_id);
+        if (!user) {
             return res.status(404).send('User not found');
         }
         res.status(200).send(user);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
+
+    // User.findById(_id).then(user => {
+    //     if(!user) {
+    //         return res.status(404).send('User not found');
+    //     }
+    //     res.status(200).send(user);
+    // }).catch(error => {
+    //     res.status(500).send(error);
+    // })
 })
 
-
-
-app.post('/tasks', (req, res) => {
+// Add new task
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
-    task.save().then((task) => {
+
+    try {
+        await task.save();
         res.status(201).send(task);
-    }).catch(e => {
-        res.status(400).send(e);
-    })
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+    // task.save().then((task) => {
+    //     res.status(201).send(task);
+    // }).catch(e => {
+    //     res.status(400).send(e);
+    // })
 })
 
+// Get all tasks
 app.get('/tasks', async (req, res) => {
-    Task.find({}).then(tasks => {
+    try {
+        const tasks = await Task.find({});
         res.status(200).send(tasks);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
+    // Task.find({}).then(tasks => {
+    //     res.status(200).send(tasks);
+    // }).catch(error => {
+    //     res.status(500).send(error);
+    // })
 })
 
-app.get('/tasks/:id', (req, res) => {
+// Get task by ID
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
-    Task.findById({ _id }).then(task => {
-        if(!task) {
+
+    try {
+        const task = await Task.findById(_id);
+        if (!task) {
             return res.status(404).send('Task not found');
         }
         res.status(200).send(task);
-    }).catch(error => {
+    } catch (error) {
         res.status(500).send(error);
-    })
+    }
+    // Task.findById(_id).then(task => {
+    //     if(!task) {
+    //         return res.status(404).send('Task not found');
+    //     }
+    //     res.status(200).send(task);
+    // }).catch(error => {
+    //     res.status(500).send(error);
+    // })
 })
 
 app.listen(port, () => {
